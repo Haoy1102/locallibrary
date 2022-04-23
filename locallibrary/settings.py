7bd960a58795bce1a82ce95fa5f2970bce08c662
Haoy1102 <hyhaoyang1@163.com>
2022-04-23 16:31:34 +0800

    部署准备

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library_db',
+        'USER': 'haoy',
+        'PASSWORD': 'Hy011102..',
+        'HOST': '120.77.68.151',
+        'PORT': '3306',
     }
 }
 
